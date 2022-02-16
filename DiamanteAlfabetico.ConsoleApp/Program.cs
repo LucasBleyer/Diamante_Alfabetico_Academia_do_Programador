142a753ad1bc1018a656f4cdcb67740999ff5db1
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-16 17:17:59 -0300

    entrada

--- DiamanteAlfabetico.ConsoleApp/Program.cs
+++ DiamanteAlfabetico.ConsoleApp/Program.cs
@@ -8,86 +8,16 @@ namespace DiamanteAlfabetico.ConsoleApp
         {
             //declaração de variaveis
             char letra;
-            string str_alfabeto = "abcdefghijklmnopqrstuvwxyz";
-            int espacos_fora, espacos_dentro = 0, metade = 0;
+            string str_alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYS";
+            int espacos_fora, espacos_dentro = 0;
 
             //entrada de dados
             Console.Write("Digite uma letra qualquer do alfabeto: ");
-            letra = Convert.ToChar(Console.ReadLine());
+            letra = Convert.ToChar(Console.ReadLine().ToUpper());
 
-            //converter a string do alfabeto em um vetor em char
+            //converter a string do alfabeto em um vetor em char {'A', 'B', 'C'...}
             char[] alfabeto = str_alfabeto.ToCharArray();
-
-            //achar o meio do diamante
-            for (int i = 0; i < alfabeto.Length; i++)
-            {
-                if (alfabeto[i] == letra)
-                {
-                    metade = i;
-                }
-            }
-
-            espacos_fora = metade;
-            for (int i = 0; i <= metade; i++)
-            {
-                for (int j = 0; j < espacos_fora; j++)
-                {
-                    Console.Write(" ");
-                }
-                Console.Write(alfabeto[i]);
-                for (int k = 0; k < espacos_dentro; k++)
-                {
-                    Console.Write(" ");
-                }
-                if (i != 0)
-                {
-                    Console.Write(alfabeto[i]);
-                }
-                Console.WriteLine();
-
-                espacos_fora = espacos_fora - 1;
-                if (i == 0)
-                {
-                    espacos_dentro = 1;
-                }
-                if (i != 0 && i != metade)
-                {
-                    espacos_dentro = espacos_dentro + 2;
-                }
-            }
-
-            espacos_fora = 0;
-            for (int i = 1; i <= metade; i++)
-            {
-
-                espacos_fora = espacos_fora + 1;
-                if (i == metade)
-                {
-                    espacos_dentro = 1;
-                }
-                if (i != 0 && i != metade)
-                {
-                    espacos_dentro = espacos_dentro - 2;
-                }
-
-                for (int j = 0; j < espacos_fora; j++)
-                {
-                    Console.Write(" ");
-                }
-
-                Console.Write(alfabeto[metade - i]);
-
-                for (int k = 0; k < espacos_dentro; k++)
-                {
-                    Console.Write(" ");
-                }
-
-                if (i != metade)
-                {
-                    Console.Write(alfabeto[metade - i]);
-                }
-                Console.WriteLine();
-            }
+            
         }
     }
 }
