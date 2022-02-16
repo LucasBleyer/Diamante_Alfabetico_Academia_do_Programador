006e394385971cec36b1878a061efa17431e9782
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-16 15:07:47 -0300

    Add project files.

---
+++ DiamanteAlfabetico.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiamanteAlfabetico.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
